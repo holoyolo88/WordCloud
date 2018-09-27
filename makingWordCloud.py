67f831fffb84e4a3ec188c5b9033bf6aa6a89228
holoyolo88 <ansoh4025@naver.com>
2018-09-27 21:56:58 +0900

    makingWordCloud module

--- makingWordCloud.py
+++ makingWordCloud.py
@@ -12,7 +12,5 @@ nouns = nouns_tagger.nouns(word_corpus)
 count = Counter(nouns)
 
 ranked_tags = count.most_common(40)
-print(ranked_tags)
 taglist = pytagcloud.make_tags(ranked_tags, maxsize=80)
-print(taglist)
 pytagcloud.create_tag_image(taglist, 'wordcloud.jpg', size=(900, 600), fontname='NanumBarunGothic', rectangular=False)
