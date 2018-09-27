94710c5bca3720fe9b2ff8fdbc7b3b3f2313f859
holoyolo88 <ansoh4025@naver.com>
2018-09-27 16:12:09 +0900

    makingWordCloud module

---
+++ makingWordCloud.py
@@ -0,0 +1,18 @@
+from konlpy.tag import Twitter
+from collections import Counter
+import pytagcloud
+
+INPUT_FILE = 'news_cleaned.txt'
+filereader = open(INPUT_FILE, 'r', newline='')
+word_corpus = str(filereader.readlines())
+filereader.close()
+
+nouns_tagger = Twitter()
+nouns = nouns_tagger.nouns(word_corpus)
+count = Counter(nouns)
+
+ranked_tags = count.most_common(40)
+print(ranked_tags)
+taglist = pytagcloud.make_tags(ranked_tags, maxsize=80)
+print(taglist)
+pytagcloud.create_tag_image(taglist, 'wordcloud.jpg', size=(900, 600), fontname='NanumBarunGothic', rectangular=False)
